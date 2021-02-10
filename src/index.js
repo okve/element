bb4bb62e337ff1eff96be6f463742e194707a33b
yunlong.wang <yunlong.wang@okcoin.com>
2021-02-10 14:32:33 +0800

    image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-export { default as Foo } from './Foo';
-export { default as Bar } from './Bar';
 export { default as Button } from './Button';
 export { default as Container } from './Container';
 export { default as Image } from './Image';
